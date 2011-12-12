c2430be6a277a787d1f344a1a1f0a55317e568dc
bkirman <ben@kirman.org>
2011-12-12 16:05:41 +0000

    bugfixes

--- challenges/views.py
+++ challenges/views.py
@@ -5,7 +5,7 @@ import simplejson
 def ticker(request):
     '''Return JSON list of recent challenges for the front page'''
     out = []
-    for ch in Challenge.objects.filter(complete=False)[:10]:
+    for ch in Challenge.objects.filter(complete=False).order_by('-created')[:10]:
         out.append({'u_id':ch.user.foursquare_id,'u_name':ch.user.first_name+' '+ch.user.last_name[0],'v_name':ch.start_name,'v_id':ch.start_venue_id})
     return HttpResponse(simplejson.dumps(out))
     
