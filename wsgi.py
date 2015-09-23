ea55571df5d4fc9fc2116c0d2ef2982d1374977c
Ben Kirman <ben@kirman.org>
2015-09-23 13:40:23 +0100

    Django1.8

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,6 @@ import sys
 sys.path.append('/var/django_apps/getlostbot')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'getlostbot.settings'
 os.environ['PYTHON_EGG_CACHE'] = '/var/django_apps/.python_egg_cache'
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+#application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
